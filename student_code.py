153e89eba4b127a4db1ab45d0831865b07b8975a
sik324 <sik324@lehigh.edu>
2025-11-01 15:57:21 -0400

    commit4

--- student_code.py
+++ student_code.py
@@ -1,21 +1,19 @@
 from collections import deque
 
-"""TraversalDigraph, which inherits from SortableDigraph and DAG which inherits from TraversalDigraph."""
-
 
 class SortableDigraph:
     def __init__(self):
         self.graph = {}
 
     def add_node(self, v):
-        """Add nodes to the created empty dict."""
+        """Add node if it doesn't exist."""
         if not isinstance(v, str):
-            raise TypeError(" must be a string.")
+            raise TypeError("Node must be a string.")
         if v not in self.graph:
-            self._graph[v] = set()
+            self.graph[v] = set()
 
     def add_edge(self, u, v):
-        """Add edges to the graph."""
+        """Add edge u → v."""
         self.add_node(u)
         self.add_node(v)
         self.graph[u].add(v)
@@ -25,45 +23,42 @@ class SortableDigraph:
         return self.graph.get(node, set())
 
     def __repr__(self):
-        """Returns the graph structure."""
         return f"{self.graph}"
 
 
 class TraversleDigraph(SortableDigraph):
     def dfs(self, s):
-        """Depth First search traversal."""
+        """Depth-First Search traversal."""
         G = self.graph
-        S, Q = set(), []
-        Q.append(s)
+        S, Q = set(), [s]
         while Q:
             u = Q.pop()
             if u in S:
                 continue
-        S.add(u)
-        yield u
-        Q.extend(G[u])
+            S.add(u)
+            yield u
+            Q.extend(G[u])
 
     def bfs(self, start):
-        """Breadth Firsst Search traversal using deque."""
+        """Breadth-First Search traversal using deque."""
         G = self.graph
         S = set()
         Q = deque([start])
-        Q.add(start)
         while Q:
-            u = Q.pop()
+            u = Q.popleft()
             if u in S:
                 continue
-        S.add(u)
-        yield u
-        for v in G[u]:
-            Q.add(v)
+            S.add(u)
+            yield u
+            for v in G[u]:
+                Q.append(v)
 
 
 class DAG(TraversleDigraph):
-    def add_node(self, u, v):
-        """Add edge between u to v if it is does'nt create a cycle."""
+    def add_edge(self, u, v):
+        """Add edge u → v only if it doesn’t create a cycle."""
         if self.path_exists(v, u):
-            raise ValueError(f"Adding edge{u} to {v} would create a cycle.")
+            raise ValueError(f"Adding edge {u} → {v} would create a cycle.")
         super().add_edge(u, v)
 
     def path_exists(self, start, target):
